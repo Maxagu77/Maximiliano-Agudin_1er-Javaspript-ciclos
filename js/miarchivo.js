5c4185c60c741021b7c2bc0e7ce77f605f636d7e
Maximiliano Agudin <maxiragudin@hotmail.com>
2022-02-23 10:35:00 -0300

    agrego function

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -3,23 +3,26 @@ console.log('Datos ingresados')
 
 let nombreIngresado = prompt("ingrese su nombre")
 let apellidoIngresado = prompt("ingrese su apellido")
-const interes = 1.02 // interes compuesto del 2% mensual
+const interes1 = 1.02  // interes compuesto del 2% mensual
 let interesTotal = 1
-
+function solicitarDatos () { 
+    alert('hola'+ ' '+ nombreIngresado + ' ' + apellidoIngresado +"\n Seleccione aceptar y a continuacion Ingrese capital a invertir")
+}
 
 if ((nombreIngresado !="") && (apellidoIngresado !="")) {
-    alert('hola'+ ' '+ nombreIngresado + ' ' + apellidoIngresado +"\n Seleccione aceptar y a continuacion Ingrese capital a invertir");
+    
+    solicitarDatos ();
     let datoCapital = prompt("Ingrese el Capital a invertir")
     let capital = parseFloat(datoCapital)
     let datoMeses = prompt("Ingrese periodo en meses que desea invertir")
     let meses = parseInt(datoMeses)
-    for (let i = 0; i < meses; i++) {
-        interesTotal = interesTotal * interes
+     for (let i = 0; i < meses; i++) {
+        interesTotal = interesTotal * interes1
     }
-
     //let capitalReintegro = capital * interesTotal
     alert('capital a reintegrar: '+ capital * interesTotal)
-}
+    }
+
 else {
     alert('ingrese su nombre y apellido')
 }
